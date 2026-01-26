fddfaa1137b88c0dc3709b5e246ae05c3ca890c7
aditya kumar das <adityacm149@gmail.com>
2026-01-26 19:21:34 +0530

    Restore ListNode definition and detectCycle method

--- season 7/142-Leatcode.hpp
+++ season 7/142-Leatcode.hpp
@@ -1,4 +1,6 @@
 
+![image alt](https://github.com/aditya-kumar-das-149/DAA_4_2025_26/blob/b7642e5031b27619ef59b25131a67f6f3da12aa4/season%207/Screenshot%202026-01-26%20185850.png)
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -27,3 +29,4 @@ public:
         return NULL;
     }
 };
+
